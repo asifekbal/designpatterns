53bab410f0072b8f2b66489d00a4dfc2ae9c1bc3
asifekbal <asif.sunny678@gmail.com>
2022-08-03 17:25:19 +0530

    first commit

---
+++ src/main/java/asif/headfirst/App.java
@@ -0,0 +1,13 @@
+package asif.headfirst;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
